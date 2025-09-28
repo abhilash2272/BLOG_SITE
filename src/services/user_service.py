20fd295dbcc27a8a3a2f94abd06e006a9438aa55
abhilash2272 <boddupallyabhilash380@gmail.com>
2025-09-28 19:26:01 +0530

    Fix user_service.py to use passlib, add init files, update requirements

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,4 +1,4 @@
-from passlib.hash import bcrypt
+from passlib.hash import bcrypt as passlib_bcrypt
 from src.dao.user_dao import UserDAO
 
 class UserService:
@@ -6,19 +6,26 @@ class UserService:
         self.dao = UserDAO()
 
     def signup(self, name, email, password):
-        hashed = bcrypt.hash(password)
-        return self.dao.create_user({"name": name, "email": email, "password": hashed})
+        """Create a new user with hashed password"""
+        hashed = passlib_bcrypt.hash(password)
+        return self.dao.create_user({
+            "name": name,
+            "email": email,
+            "password": hashed
+        })
 
     def login(self, email, password):
+        """Verify user login"""
         user = self.dao.get_user_by_email(email)
-        if user and bcrypt.verify(password, user["password"]):
+        if user and passlib_bcrypt.verify(password, user["password"]):
             return user
         return None
 
     def update_profile(self, user_id, name=None, password=None):
+        """Update user profile"""
         data = {}
         if name:
             data["name"] = name
         if password:
-            data["password"] = bcrypt.hash(password)
+            data["password"] = passlib_bcrypt.hash(password)
         return self.dao.update_user(user_id, data)
